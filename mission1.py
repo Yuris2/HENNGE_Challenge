09890f76904ddfef874efdc007feab80af8d7b1e
Yuri Batan <batanyuri@gmail.com>
2026-04-23 18:49:00 -0400

    Problem Statement

---
+++ mission1.py
@@ -0,0 +1,9 @@
+#Input is a list of integers separated by new line character
+#First line of input will be N s.t 
+#   (1 <= N <= 100) indicating. num of test cases to  follow
+#Each of the test cases will consist of a line with an integer X s.t
+#   (0<X<= 100) followed by another line consisting f X number of space-sep ints Yn s.t
+        #Y.n (-100 <= Yn <= 100)
+
+#For each test case, calculate power of four of Yn (excluding when Yn > 0)
+#Print calcualted sum in the output
