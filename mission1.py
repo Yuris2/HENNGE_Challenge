5209adc0ddd795a81ecee1f26e7807d26701ce7b
Yuri Batan <batanyuri@gmail.com>
2026-04-23 18:52:44 -0400

    Pseudocode for solution

--- mission1.py
+++ mission1.py
@@ -7,3 +7,18 @@
 
 #For each test case, calculate power of four of Yn (excluding when Yn > 0)
 #Print calcualted sum in the output
+
+#Solution 
+
+#1. Read number of test cases first
+#2. Store results to print everything at the end
+
+#3. Iterate over each test case 
+    #4. Read X, which is the expected number of ints
+    #5. Read the next line and split into list of ints
+    #6. If count of integers != X, Store -1
+    #7. Otherwise, filter out integers > 0
+    #8. Raise each remaining number to 4th power and sum them
+    #9. Store the result
+
+#10. After all test cases, print each result on individual line
